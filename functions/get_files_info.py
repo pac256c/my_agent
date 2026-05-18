23481eaf6bc48abca9ffee28299b1006db846354
pac256c <peter8pie@gmail.com>
2026-05-18 00:55:58 -0400

    changes to get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -9,7 +9,7 @@ def get_files_info(working_directory, directory="."):
         valid_target_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
 
         #list contents of directory, error 
-        if not valid_target_dir: return f'Error: Cannot list "{target_dir}" as it is outside the permitted working directory\n'
+        if not valid_target_dir: return f'Error: Cannot list "{directory}" as it is outside the permitted working directory\n'
         if not os.path.isdir(target_dir): return f'Error: "{target_dir}" is not a directory\n'
 
         #list files/directories in the directory specified
